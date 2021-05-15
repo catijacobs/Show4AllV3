f2b04f71fd1cdf176f60108be006f7f81a805cf7
catiajacob <catia.jacob@inovflow.pt>
2021-05-15 20:59:26 +0100

    set role controller and views

--- Show4AllV3/Controllers/RoleController.cs
+++ Show4AllV3/Controllers/RoleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,25 @@ namespace Show4AllV3.Controllers
 {
     public class RoleController : Controller
     {
+        RoleManager<IdentityRole> roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
         }
     }
 }
